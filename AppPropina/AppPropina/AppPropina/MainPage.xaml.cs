ba9004cb5bb3dfff2e35a80ab9b6068e2feef1ac
elkinaldo1 <38057843+elkinaldo1@users.noreply.github.com>
2018-04-11 19:56:40 -0500

    calcular

--- AppPropina/AppPropina/AppPropina/MainPage.xaml.cs
+++ AppPropina/AppPropina/AppPropina/MainPage.xaml.cs
@@ -7,11 +7,27 @@ using Xamarin.Forms;
 
 namespace AppPropina
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnCalcular.Clicked += BtnCalcular_Clicked;
+        }
+
+        private void BtnCalcular_Clicked(object sender, EventArgs e)
+        {
+            var total = decimal.Parse(entTotal.Text);
+            var propina = decimal.Parse(entPropina.Text);
+            var numpersonas = decimal.Parse(entNumPersona.Text);
+
+            var totalPropina = total * propina;
+            var totalC = total + totalPropina;
+
+            txtPropina.Detail = (totalPropina).ToString("C");
+            txtTotal.Detail = (totalC).ToString("C");
+            txtPropinaPersona.Detail = (totalPropina / numpersonas).ToString("C");
+            txtTotalPropina.Detail = (totalC / numpersonas).ToString("C");            
+        }
+    }
 }
